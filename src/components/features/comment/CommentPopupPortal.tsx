ccd161dc47d5f0d55571fd61fcfeec50b00b585a
Amon3141 <kizawaamon@gmail.com>
2025-06-15 15:20:38 -0700

    deleted unnecessary filders

--- src/components/features/comment/CommentPopupPortal.tsx
+++
@@ -1,6 +0,0 @@
-import { createPortal } from "react-dom";
-
-export default function CommentPopupPortal({ children }: { children: React.ReactNode }) {
-  if (typeof window === "undefined") return null;
-  return createPortal(children, document.body);
-}
